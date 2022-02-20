56bc6c7ac0bf3c73c79de6bf7f8d92613f1e119e
Maykel Santos <maykelsantoz@gmail.com>
2022-02-20 19:43:33 -0300

    Chalenge Node.js 01  complete.

--- src/index.js
+++ src/index.js
@@ -8,16 +8,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-/**
- * Tipos de params => Identificar um recurso editar/deletar/buscar
- * 
- */
-
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   const { username } = request.headers;
-  //const { username } = 'maykelsantoz';
 
   const user = users.find((user) => user.username === username);
 
@@ -42,90 +36,79 @@ app.post('/users', (request, response) => {
     return response.status(400).json({ error: "Username already exists!" });
   }
 
-  users.push({
+  const user = {
     id: uuidv4(),
     name,
     username,
     todos: []
-  });
+  }
 
-  return response.status(201).send();
+  users.push(user);
+
+  return response.status(201).json(user);
 
 });
 
-/**Delete */
-// app.get('/users', (request, response) => {
-//   return response.status(201).json(users);
-// });
-/**Delete */
-
-
 app.get('/todos', checksExistsUserAccount, (request, response) => {
   const { user } = request;
-
   return response.json(user.todos);
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
+  const { user } = request;
   const { title, deadline } = request.body;
 
-  const { user } = request;
 
   const todosAssignment = {
     id: uuidv4(),
     title,
     done: false,
     deadline: new Date(deadline),
-    created_at: new Date(),
+    created_at: new Date()
   }
 
   user.todos.push(todosAssignment);
 
-  return response.status(201).send();
+  return response.status(201).json(todosAssignment);
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
+  const { user } = request;
   const { title, deadline } = request.body;
   const { id } = request.params;
-  const { user } = request;
 
-  const todo = user.todos.find((todos) => todos.id === id);
+  const todo = user.todos.findIndex((todos) => todos.id === id);
 
-  if (!todo) {
+  if (todo < 0) {
     return response.status(404).json({ error: "Todo id not found!" });
   }
 
-  todo.title = title;
-  todo.deadline = new Date(deadline);
+  const todoUpdate = user.todos[todo]
 
-  return response.status(201).send();
+  title ? todoUpdate.title = title : false;
+  deadline ? todoUpdate.deadline = new Date(deadline) : false;
+
+  return response.status(201).json(todoUpdate);
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  //const { title, deadline } = request.body;
-  const { id } = request.params;
   const { user } = request;
+  const { id } = request.params;
 
-  const todo = user.todos.find((todos) => todos.id === id);
+  const todo = user.todos.findIndex((todos) => todos.id === id);
 
-
-  if (!todo) {
+  if (todo < 0) {
     return response.status(404).json({ error: "Todo id not found!" });
   }
 
-  if (todo.done === false) {
-    todo.done = true
-  } else {
-    todo.done = false
-  }
+  user.todos[todo].done = true
 
-  return response.status(201).send();
+  return response.status(201).json(user.todos[todo]);
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
-  //const { title, deadline } = request.body;
-  const { id } = request.params;
   const { user } = request;
+  const { id } = request.params;
 
   const todo = user.todos.find((todos) => todos.id === id);
 
@@ -135,7 +118,7 @@ app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
 
   user.todos.splice(todo, 1);
 
-  return response.status(200).json(users);
+  return response.status(204).send();
 
 });
 
